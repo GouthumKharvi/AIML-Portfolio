73b59755361e02dc196c3626c84df585dfd9e711
gouthum Kharvi <gouthumkharvi1899@gmail.com>
2026-03-26 04:49:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/AIML-Portfolio/",   // 👈 THIS LINE IS CRITICAL
   plugins: [react()],
 });
